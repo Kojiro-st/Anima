55dc15489c7bceae130911c9dc08cd03607e6890
Kojiro-st <tenmadetobe5a@gmail.com>
2020-06-03 23:07:43 +0900

    名前変更・vue router導入

---
+++ app/javascript/packs/components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <nav>
+      <h1>フッターでーす</h1>
+    </nav>
+  </div>
+</template>
---
+++ app/javascript/packs/router/router.js
@@ -0,0 +1,14 @@
+// import Vue from 'vue/dist/vue.esm.js'
+// import VueRouter from 'vue-router'
+// import Body from './components/body.vue'
+// import Fistfooter from './components/firstfooter.vue'
+
+// Vue.use(VueRouter)
+
+// export default new VueRouter({
+//   mode: 'history',
+//   routes: [
+//     { path: '/body', component: body },
+//     { path: '/about', component: About },
+//   ],
+// })
