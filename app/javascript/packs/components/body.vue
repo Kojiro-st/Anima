103b16452be482b8375770b933ffa9b476dc895e
Kojiro-st <tenmadetobe5a@gmail.com>
2020-06-04 16:14:54 +0900

    Revert "名前変更・vue router導入"
    This reverts commit 55dc15489c7bceae130911c9dc08cd03607e6890.

--- app/javascript/packs/components/body.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <nav>
-      <h1>お腹</h1>
-    </nav>
-  </div>
-</template>
--- app/javascript/packs/components/footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <nav>
-      <h1>フッターでーす</h1>
-    </nav>
-  </div>
-</template>
