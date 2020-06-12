236072664adf18f5b8a800b26a9c64476ec78bc9
Kojiro-st <tenmadetobe5a@gmail.com>
2020-06-12 14:16:22 +0900

    要素の書き出し完了

--- app/javascript/packs/components/firstfooter.vue
+++ app/javascript/packs/components/firstfooter.vue
@@ -1,14 +1,51 @@
 <template>
-  <div>
-    <div class="bottom-top">
-      <div class="bottom-top-left">
-        <p class="brand-logo left"><i class="material-icons">all_inclusive</i>Anima</p>
+  <div class="footer">
+      <div class="footer-in">
+      <div class="bottom-top">
+        <div class="bottom-top-left">
+          <i class="fab fa-spotify text-white"><span>Anima</span></i>
+        </div>
+        <div class="bottom-top-right">
+          <p class="text-muted">会社情報</p>
+          <ul>
+            <li><span><a href="#" class="text-white">企業情報</a></span></li>
+            <li><span><a href="#" class="text-white">採用情報</a></span></li>
+            <li><span><a href="#" class="text-white">for the record</a></span></li>
+          </ul>
+          <ul>
+            <li><i class="fab fa-instagram text-white"></i></li>
+            <li><span><i class="fab fa-twitter text-white"></i></span></li>
+            <li><i class="fab fa-facebook-f text-white"></i></li>
+          </ul>
+        </div>
       </div>
-      <div class="bottom-top-right"></div>
-    </div>
-    <div class="bottom-bottom">
-      <div class="bottom-bottom-left"></div>
-      <div class="bottom-bottom-right"></div>
+      <div class="bottom-bottom">
+        <div class="bottom-bottom-left">
+          <ul>
+            <li><span><a href="#" class="text-white">法的情報</a></span></li>
+            <li><span><a href="#" class="text-white">Cookie</a></span></li>
+            <li><span><a href="#" class="text-white">プライバシー</a></span></li>
+          </ul>
+        </div>
+        <div class="bottom-bottom-right">
+          <p class="text-white">© 2020 Anima AB</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.footer{
+  background-color: black;
+}
+
+.footer-in{
+  width: 80%;
+  margin: 0 auto;
+}
+
+.bottom-top-left{
+  font-size: 25px
+}
+</style>
