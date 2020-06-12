bad94b6e6628be0bf2f7055ac2b6ee3a2ea95eb7
Kojiro-st <tenmadetobe5a@gmail.com>
2020-06-12 13:28:36 +0900

    font-awesoem

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>
-    <h1>いいね</h1>
+  <div class="header text-white">
+    <div class="header-left font-weight-bold">
+      <i class="fab fa-spotify"><span>Anima</span></i>
+    </div>
+    <div class="header-right">
+      <a href="#" class="text-white premium">Premium</a>
+      <a href="#" class="text-white premium">新規登録・ログイン</a>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.header{
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 100px;
+  background-color:black; 
+}
+
+.header-left{
+  line-height: 80px;
+  font-size: 30px;
+}
+
+.header-right{
+  line-height: 80px;
+  font-size: 20px;
+}
+.premium{
+  padding-right: 20px;
+  text-decoration: none;
+}
 </style>
